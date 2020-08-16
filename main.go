c2964f07e1c7a33ee1e83e54386a39b2eb92afb7
sitetester <sitetester2010@gmail.com>
2020-08-16 14:36:44 +0300

    custom file added in /resources directory

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import "github.com/sitetester/recipe-stats-calculator/service/calculator"
 func main() {
 
 	var r calculator.RecipeStatsCalculator
-	r.CalculateStats("./tmp/hf_test_calculation_fixtures_SMALL.json",
+	r.CalculateStats("./resources/hf_test_calculation_fixtures_SMALL.json",
 		calculator.PostcodeDeliveryTimeFilter{
 			Postcode: "10120",
 			FromAM:   10,
